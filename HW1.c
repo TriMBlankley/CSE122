a81407e1aa3151d197f50a5fccb59398b2ec83a5
TriMBlankley <triblankley@gmail.com>
2022-02-01 16:48:46 -0700

    made HW1 for cse122

---
+++ HW1.c
@@ -0,0 +1,39 @@
+/**
+ *@author: Tri Blankely
+ *@date: Jan 31 2021
+ *@file: HW1.c
+ *
+ *@breif: 
+ */
+
+
+#include <stdio.h> 
+#include <stdlib.h> 
+
+/**
+ * @brief Initilizes array, and calls all sub functions
+ * 
+ * @return int 
+ */
+int main(void) 
+{ 
+	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
+
+	printf("Given array is \n"); 
+
+
+	return 0; 
+} 
+
+
+/**
+ * @brief 
+ * @param arr: Input  seqential if rotated array
+ * @param obj:number the function is searching for
+ * 
+ * @return int valule of position of Obj in array, if obj !present, return -1
+ */
+void finder(int arr[], int Obj) 
+{ 
+    printf("test");
+}
